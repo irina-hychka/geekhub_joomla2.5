ca2f08ddd0a9e8df0f9948faef7bbe5c837adf36
iryna.gychka <iryna.gychka@masterofcode.com>
2013-10-16 14:15:19 +0300

    new test project on joomla2.5

---
+++ logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2013-10-16 08:16:19 UTC
+#Software: Joomla Platform 11.4.0 Stable [ Brian Kernighan ] 03-Jan-2012 00:00 GMT
+
+#Fields: date	time	priority	clientip	category	message
+2013-10-16	08:16:19	INFO	127.0.0.1	Joomla FAILURE: 	Empty password not allowed
+2013-10-16	08:16:25	INFO	127.0.0.1	Joomla FAILURE: 	Username and password do not match or you do not have an account yet.
